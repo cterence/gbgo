36a73bb8596a861553d6c172a3d0e3a7e1cf514f
Térence Chateigné <terence.chateigne@posteo.net>
2025-12-17 01:33:38 +0100

    fix: serial transfer behavior

--- internal/console/components/serial/serial.go
+++ internal/console/components/serial/serial.go
@@ -6,7 +6,10 @@ const (
 	SB = 0xFF01
 	SC = 0xFF02
 
-	SERIAL_CYCLES = 8192
+	// 4,194,304 / 8,192 = 512 CPU cycles per bit
+	// 512 * 8 = 4,096 CPU cycles per byte transfer
+	// It accounts for the hardware bit shifting mechanism
+	SERIAL_CYCLES = 4096
 
 	INTERRUPT_CODE = 0x8
 )
@@ -44,19 +47,20 @@ func (s *Serial) Init(options ...Option) {
 }
 
 func (s *Serial) Step(cycles int) {
+	if s.sc&0x81 != 0x81 {
+		return
+	}
+
 	s.cycles += cycles
-
 	if s.cycles >= SERIAL_CYCLES {
-		if s.sc&0x80 != 0 {
-			if s.print {
-				fmt.Print(string(s.sb))
-			}
-
-			s.sc &= 0x7F
-			s.CPU.RequestInterrupt(INTERRUPT_CODE)
+		if s.print {
+			fmt.Print(string(s.sb))
 		}
 
-		s.cycles -= SERIAL_CYCLES
+		s.sb = 0xFF
+		s.sc &= 0x7F
+		s.cycles = 0
+		s.CPU.RequestInterrupt(INTERRUPT_CODE)
 	}
 }
 
